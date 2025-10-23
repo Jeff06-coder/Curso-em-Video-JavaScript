b653a33081a02465dd63f7b6cbc478874877071e
Jeff06-coder <loplopado67@gmail.com>
2025-10-23 10:30:09 -0300

    Aula 12 feita,uso do switch e if else, condições aninhadas e multipla.

--- Aula 12/node.js
+++ Aula 12/node.js
@@ -10,14 +10,14 @@ if (idade < 16) {
 }
 
 
-var agr =  new Date()
+var agr = new Date()
 var hora = agr.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
-if (hora >= 0 && hora < 5 ) {
+if (hora >= 0 && hora < 5) {
     console.log(`Vai dormir!`)
 } else if (hora < 12 && hora >= 5) {
     console.log(`Bom dia!`)
-} else if (hora >= 12 && hora < 18){
+} else if (hora >= 12 && hora < 18) {
     console.log(`Boa tarde!`)
 } else {
     console.log(`Boa noite!`)
